5626fcbd051efc6916ea6b2f014ef4cd74bf36e0
MongLong0214 <weplay0628@gmail.com>
2022-07-11 20:27:33 +0900

    portfolio_3rd_page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Header from "../header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faJsSquare } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+
 import {
   FirstPage,
   FirstPageProfileImg,
@@ -11,8 +16,32 @@ import {
   SecondPageSubCopy,
   SecondPageSubCopy2,
   SecondPageSubCopy3,
+  SecondPageCircleBox,
+  SecondPageEachCircleBox,
+  SecondPageCopyBox,
+  SecondPageCircle,
+  SecondPageCareer,
+  SecondPageCareerText,
+  ThirdPage,
+  ThirdPageHeadCopy,
+  ThirdPageSubCopy,
+  ThirdPageSkillBoxContainer,
+  ThirdPageSkillBox,
+  ThirdPageSkillItem,
+  ThirdPageSkillCircle,
+  StyledFontAwesomeIcon,
+  ThirdPageSkillTextBox,
+  ThirdPageSkillSubTextBox,
+  ThirdPageSkillBox2,
+  StyledFontAwesomeIcon2,
 } from "./HomeStyle";
 import ProfileImg from "../../img/ProfileImg.jpg";
+import HTML from "../../img/HTML.png";
+import JS from "../../img/JS.png";
+import REACT from "../../img/REACT.png";
+
+import imoge1 from "../../img/imoge1.png";
+import imoge2 from "../../img/imoge2.png";
 
 const Home = () => {
   return (
@@ -35,7 +64,90 @@ const Home = () => {
         <SecondPageSubCopy3>
           능동적인 프론트엔드 개발자 조원일 입니다.
         </SecondPageSubCopy3>
+        <SecondPageCircleBox>
+          <SecondPageEachCircleBox>
+            <SecondPageCircle>
+              <StyledFontAwesomeIcon icon={faHtml5} />
+            </SecondPageCircle>
+            <SecondPageCopyBox>
+              HTML/CSS를 Front-End 개발에 <br />
+              적합하게 작성합니다.
+            </SecondPageCopyBox>
+          </SecondPageEachCircleBox>
+          <SecondPageEachCircleBox>
+            <SecondPageCircle>
+              <StyledFontAwesomeIcon icon={faJsSquare} />
+            </SecondPageCircle>
+            <SecondPageCopyBox>
+              React를 더 잘 다루기 위해
+              <br /> Vanila Js를 중요하게 생각하고 있습니다.
+            </SecondPageCopyBox>
+          </SecondPageEachCircleBox>
+          <SecondPageEachCircleBox>
+            <SecondPageCircle>
+              <StyledFontAwesomeIcon icon={faReact} />
+            </SecondPageCircle>
+            <SecondPageCopyBox>
+              React를 이용한
+              <br /> 협업 프로젝트 경험이 4회 있습니다.
+            </SecondPageCopyBox>
+          </SecondPageEachCircleBox>
+        </SecondPageCircleBox>
+        <SecondPageCareer>
+          <SecondPageCareerText>
+            비디오샵 (광고제작프로덕션) - 광고 제작 PD{" "}
+            <span style={{ "font-size": "0.5rem" }}>(2020.01~2021.08) </span>
+          </SecondPageCareerText>
+          <SecondPageCareerText>
+            Social MC (광고 대행사) - 영상 광고 기획 및 PM{" "}
+            <span style={{ "font-size": "0.5rem" }}>(2018.06~2019.10) </span>
+          </SecondPageCareerText>
+          <SecondPageCareerText>
+            준콤 (광고대행사) - 광고 영업 및 AE{" "}
+            <span style={{ "font-size": "0.5rem" }}>(2017.01~2018.03) </span>
+          </SecondPageCareerText>
+        </SecondPageCareer>
       </SecondPage>
+      <ThirdPage>
+        <ThirdPageHeadCopy>Skills</ThirdPageHeadCopy>
+        <ThirdPageSubCopy>Skills & Attributes </ThirdPageSubCopy>
+        <ThirdPageSkillBoxContainer>
+          <ThirdPageSkillBox>
+            <ThirdPageSkillItem>
+              <ThirdPageSkillCircle>
+                <StyledFontAwesomeIcon2 icon={faHtml5} />
+              </ThirdPageSkillCircle>
+              <ThirdPageSkillTextBox>
+                Html / Css
+                <ThirdPageSkillSubTextBox>
+                  ( styled-component )
+                </ThirdPageSkillSubTextBox>
+              </ThirdPageSkillTextBox>
+            </ThirdPageSkillItem>
+            <ThirdPageSkillItem>
+              <ThirdPageSkillCircle>
+                <StyledFontAwesomeIcon2 icon={faJsSquare} />
+              </ThirdPageSkillCircle>
+              <ThirdPageSkillTextBox>
+                JavaScript
+                <ThirdPageSkillSubTextBox>
+                  ( TypeScript )
+                </ThirdPageSkillSubTextBox>
+              </ThirdPageSkillTextBox>
+            </ThirdPageSkillItem>
+            <ThirdPageSkillItem>
+              <ThirdPageSkillCircle>
+                <StyledFontAwesomeIcon2 icon={faReact} />
+              </ThirdPageSkillCircle>
+              <ThirdPageSkillTextBox style={{ marginLeft: "4rem" }}>
+                React
+                <ThirdPageSkillSubTextBox>( recoil )</ThirdPageSkillSubTextBox>
+              </ThirdPageSkillTextBox>
+            </ThirdPageSkillItem>
+          </ThirdPageSkillBox>
+          <ThirdPageSkillBox2></ThirdPageSkillBox2>
+        </ThirdPageSkillBoxContainer>
+      </ThirdPage>
     </>
   );
 };
--- src/components/home/HomeStyle.js
+++ src/components/home/HomeStyle.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const FirstPage = styled.section`
   width: 100%;
@@ -53,29 +54,13 @@ export const ContactButton = styled.button`
 `;
 
 export const SecondPage = styled(FirstPage)`
-  height: 100vh;
+  height: auto;
   background-color: #19c4eb;
 `;
 
-// export const SecondPageCopyBox = styled.div`
-//   width: 28vw;
-//   height: 40vh;
-
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-
-//   font-size: 2rem;
-//   font-weight: bold;
-//   color: #7579da;
-
-//   background-color: black;
-// `;
-
 export const SecondPageHeadCopy = styled.div`
-  height: 20vh;
-
+  /* height: 20vh; */
+  margin-top: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -87,6 +72,7 @@ export const SecondPageHeadCopy = styled.div`
 `;
 export const SecondPageSubCopy = styled.div`
   margin-bottom: 1rem;
+  margin-top: 1rem;
 
   display: flex;
   justify-content: center;
@@ -94,7 +80,7 @@ export const SecondPageSubCopy = styled.div`
   text-align: center;
   font-weight: bold;
   /* background-color: red; */
-  font-size: 1.3rem;
+  font-size: 1.7rem;
   color: #dcdf50;
 `;
 
@@ -105,10 +91,10 @@ export const SecondPageSubCopy2 = styled.div`
   text-align: center;
   font-weight: bold;
   /* background-color: red; */
-  font-size: 1.3rem;
+  font-size: 1.7rem;
   color: #aa78bc;
 
-  margin-bottom: 2rem;
+  margin-bottom: 2.5rem;
 `;
 
 export const SecondPageSubCopy3 = styled.div`
@@ -117,7 +103,193 @@ export const SecondPageSubCopy3 = styled.div`
   align-items: center;
   text-align: center;
   /* background-color: gray; */
-  font-size: 2.7rem;
+  font-size: 2.5rem;
   font-weight: bold;
   color: #dcdf50;
+`;
+
+export const SecondPageCircleBox = styled.div`
+  margin-top: 2rem;
+  width: 90%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  /* background-color: gray; */
+`;
+export const SecondPageEachCircleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* background-color: gray; */
+`;
+
+export const SecondPageEachCircleIMG = styled.img`
+  &:hover {
+    transform: rotate(-15deg);
+  }
+`;
+
+export const SecondPageCopyBox = styled.div`
+  margin-top: 2rem;
+
+  font-weight: bold;
+  font-size: 1.4rem;
+  line-height: 2.5rem;
+  text-align: center;
+
+  color: #dcdf50;
+
+  display: flex;
+
+  align-items: center;
+  /* background-color: blue; */
+`;
+
+export const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  color: #dcdf50;
+  font-size: 10rem;
+`;
+
+export const SecondPageCircle = styled.div`
+  box-sizing: border-box;
+  border-radius: 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20rem;
+  height: 20rem;
+  background: #19c4eb;
+  border: 5px solid #8ce2f5;
+
+  &:hover {
+    background-color: #dcdf50;
+    ${StyledFontAwesomeIcon} {
+      color: #19c4eb;
+      transform: rotate(-15deg);
+    }
+  }
+`;
+
+export const SecondPageCareer = styled.div`
+  /* justify-content: flex-start;
+  align-items: flex-start; */
+  margin-top: 3rem;
+  /* background: blue; */
+
+  border-left: 0.3rem solid #8ce2f5;
+  margin-bottom: 3rem;
+`;
+export const SecondPageCareerText = styled.div`
+  text-align: left;
+  margin-left: 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  color: #dcdf50;
+`;
+
+export const ThirdPage = styled.section`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #e3c53d;
+`;
+
+export const ThirdPageHeadCopy = styled(SecondPageHeadCopy)`
+  /* height: 20vh; */
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  /* background-color: blue; */
+  font-size: 5rem;
+  font-weight: bold;
+  color: #7579da;
+`;
+export const ThirdPageSubCopy = styled(SecondPageSubCopy)`
+  margin-bottom: 1rem;
+  margin-top: 1rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-weight: bold;
+  /* background-color: red; */
+  font-size: 1.7rem;
+  color: #7579da;
+`;
+
+export const ThirdPageSkillBoxContainer = styled.div`
+  display: flex;
+  margin-top: 2rem;
+  margin-bottom: 4rem;
+`;
+
+export const ThirdPageSkillBox = styled.div`
+  width: 60%;
+  /* height: 65vh; */
+  background: #e3c53d;
+  border: 3px solid #c3db64;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+`;
+
+export const ThirdPageSkillItem = styled.div`
+  /* width: 40vw; */
+  /* height: 65vh; */
+
+  display: flex;
+  align-items: center;
+  /* background: black; */
+`;
+
+export const ThirdPageSkillTextBox = styled.div`
+  color: #7579da;
+  font-size: 3rem;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  /* background: red; */
+`;
+
+export const ThirdPageSkillSubTextBox = styled.div`
+  color: #7579da;
+  font-size: 1rem;
+
+  /* background: blue; */
+`;
+export const ThirdPageSkillBox2 = styled.div`
+  width: 40vw;
+  /* height: 65vh; */
+  background: #e3c53d;
+  border: 3px solid #c3db64;
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  border-left: none;
+`;
+
+export const ThirdPageSkillCircle = styled.div`
+  width: 10rem;
+  height: 10rem;
+
+  border: 4px solid #f1e29e;
+
+  border-radius: 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 3rem;
+`;
+
+export const StyledFontAwesomeIcon2 = styled(FontAwesomeIcon)`
+  color: #7579da;
+  font-size: 6rem;
 `;
